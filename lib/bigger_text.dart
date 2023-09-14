a4a8c578304b73a9c896c654e12661be5f24e00e
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-09-14 11:32:11 +0700

    push

---
+++ lib/bigger_text.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_1/bigger_text.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+/*class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
@@ -22,17 +23,55 @@ class MyApp extends StatelessWidget {
         ),
         body: const Center(
           child: Text.rich(
-            textAlign: TextAlign.center,
-            TextSpan(text: 'Hy Saya\n', children: <TextSpan>[
-            TextSpan(
-                text: 'Flutter ',
-                style: TextStyle(fontStyle: FontStyle.italic)),
-            TextSpan(
-                text: 'Developer ',
-                style: TextStyle(fontWeight: FontWeight.bold)),
-          ])),
+              textAlign: TextAlign.center,
+              TextSpan(text: 'Hy Saya\n', children: <TextSpan>[
+                TextSpan(
+                    text: 'Flutter ',
+                    style: TextStyle(fontStyle: FontStyle.italic)),
+                TextSpan(
+                    text: 'Developer ',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ])),
+        ),
+      ),
+    );
+  }
+}*/
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: const Scaffold(
+        body: Center(
+          //child: Heading(text: "Hello world!"),
+          child: BiggerText(text: "Hello world!"),
         ),
       ),
     );
   }
 }
+
+class Heading extends StatelessWidget { // statelessWidget force state to be final
+  final String text;
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+
